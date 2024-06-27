e9033c7baaed2621879d357b01a789b740b7f01b
Andre Kurniawan <drekunia@gmail.com>
2024-06-27 12:41:49 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css"
+import { PageHeader } from "./layouts/PageHeader"
+import { CategoryPills } from "./components/CategoryPills"
+import { categories } from "./data/home"
+
+export default function App() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="sticky top-0 bg-white z-10 pb-4">
+          <CategoryPills categories={categories} />
+        </div>
+      </div>
+    </div>
+  )
+}
