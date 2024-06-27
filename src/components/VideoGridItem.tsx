fa698b3d6163ac467ed51a488b327aee8b4b3865
Andre Kurniawan <drekunia@gmail.com>
2024-06-27 18:42:07 +0700

    play video on hover

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react"
 import { formatDuration } from "../utils/formatDuration"
 import { formatTimeAgo } from "../utils/formatTimeAgo"
 
@@ -30,16 +31,45 @@ export function VideoGridItem({
   thumbnailUrl,
   videoUrl,
 }: VideoGridItemProps) {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    if (videoRef.current == null) return
+
+    if (isVideoPlaying) {
+      videoRef.current.currentTime = 0
+      videoRef.current.play()
+    } else {
+      videoRef.current.pause()
+    }
+  }, [isVideoPlaying])
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2"
+      onMouseEnter={() => setIsVideoPlaying(true)}
+      onMouseLeave={() => setIsVideoPlaying(false)}
+    >
       <a href={`/watch?v=${id}`} className="relative aspect-video">
         <img
           src={thumbnailUrl}
-          className="block w-full h-full object-cover rounded-xl"
+          className={`block w-full h-full object-cover transistion-[border-radius] duration-200 ${
+            isVideoPlaying ? "rounded-none" : "rounded-xl"
+          }`}
         />
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-gray-200 text-sm px-0.5 rounded">
           {formatDuration(duration)}
         </div>
+        <video
+          className={`block h-full object-cover rounded-xl inset-0 absolute transition-opacity duration-200 delay-200 ${
+            isVideoPlaying ? "opacity-100 delay-200" : "opacity-0"
+          }`}
+          ref={videoRef}
+          muted
+          playsInline
+          src={videoUrl}
+        ></video>
       </a>
       <div className="flex gap-2">
         <a href={`/@${channel.id}`} className="flex-shrink-0">
